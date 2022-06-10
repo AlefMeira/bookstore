daef4fbfe68a939fde687011623d09e3c0642607
alefmeira <alef.chaves01@gmail.com>
2022-06-10 16:22:45 -0300

    developed DTO for author class

--- src/main/java/com/project/bookstore/author/dto/AuthorDTO.java
+++ src/main/java/com/project/bookstore/author/dto/AuthorDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -23,6 +24,6 @@ public class AuthorDTO {
     private String name;
 
     @NotNull
-    @Size(max = 120)
-    private int age;
+    @Max(120)
+    private Integer age;
 }
